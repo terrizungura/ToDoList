e618c9281e63279a16bcc584ff5a6c2e964d5782
terrizungura <terrizun@gmail.com>
2020-04-09 08:45:53 +0200

    Implemented a date picker for the ToDo date

--- app/src/main/java/com/tererai/todolist/NewToDoActivity.java
+++ app/src/main/java/com/tererai/todolist/NewToDoActivity.java
@@ -2,15 +2,18 @@ package com.tererai.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import butterknife.BindView;
@@ -20,13 +23,15 @@ import butterknife.OnClick;
 public class NewToDoActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.android.todolist.REPLY";
-    public static final String EXTRA_DETAIL = "com.example.android.todolist.REPLY";
-    public static final String EXTRA_DATE = "com.example.android.todolist.REPLY";
+    public static final String EXTRA_DETAIL = "com.example.android.todolist.DETAIL";
+    public static final String EXTRA_DATE = "com.example.android.todolist.DATE";
 
     @BindView(R.id.edit_todo) EditText mEditToDoView;
     @BindView(R.id.edit_todo_detail) EditText mEditToDoDetail;
     @BindView(R.id.edit_todo_date) EditText mEditToDoDate;
     @BindView(R.id.button_save) Button button;
+
+    DatePickerDialog picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +43,19 @@ public class NewToDoActivity extends AppCompatActivity {
         mEditToDoDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(NewToDoActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                mEditToDoDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
             }
         });
 
